c772e54c01e2896ba0bb5a5c002aa3d11d0c0683
Gonzo04 <riccardojk47@gmail.com>
2026-04-28 17:22:29 +0200

    connessione a mongoDB tramite URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
+async function connectDB(){
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/trentoparking';
+await mongoose.connect(uri);
 
-async function connectDB() {
-  const user = process.env.MONGO_APP_USER || '';
-  const pass = process.env.MONGO_APP_PASS || '';
-  const host = process.env.MONGO_HOST || 'localhost';
-  const port = process.env.MONGO_PORT || '27017';
-  const db = process.env.MONGO_DB || 'trentoparking';
-  const authDb = process.env.MONGO_AUTH_DB || db;
+const dbName = mongoose.connection.name;
+const host = mongoose.connection.host;
 
-  const auth = user && pass ? `${encodeURIComponent(user)}:${encodeURIComponent(pass)}@` : '';
-  const authQuery = user && pass ? `?authSource=${authDb}` : '';
-  const uri = `mongodb://${auth}${host}:${port}/${db}${authQuery}`;
+console.log('MongoDB connected: ${dbName}@${host}');
 
-  await mongoose.connect(uri);
-  console.log(`MongoDB connected: ${db}@${host}:${port}`);
 }
+module.exports = connectDB
 
-module.exports = connectDB;
+
