550708ccd337fe60dec485d13e6b44596064e956
Gonzo04 <riccardojk47@gmail.com>
2026-04-28 17:53:22 +0200

    Configura MongoDB tramite URI condivisa

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-async function connectDB(){
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/trentoparking';
-await mongoose.connect(uri);
 
-const dbName = mongoose.connection.name;
-const host = mongoose.connection.host;
+async function connectDB() {
+  const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/trentoparking';
 
-console.log('MongoDB connected: ${dbName}@${host}');
+  await mongoose.connect(uri);
 
+  const dbName = mongoose.connection.name;
+  const host = mongoose.connection.host;
+
+  console.log(`MongoDB connected: ${dbName}@${host}`);
 }
-module.exports = connectDB
 
-
+module.exports = connectDB;
